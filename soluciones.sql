e1ecf302afe03321865942790cbb9e49acef334b
lorescaloni <lorena.scaloni@fce.uncu.edu.ar>
2026-08-09 22:51:45 -0300

    Update soluciones.sql

--- soluciones.sql
+++ soluciones.sql
@@ -1,20 +1,67 @@
 -- ══════════════════════════════════════════
 -- MiniStore — Soluciones con Outer JOINs
--- Autor: [Tu nombre]
--- Fecha: [Fecha de entrega]
+-- Autor: LORENA SCALONI
+-- Fecha: 09/08/26
 -- ══════════════════════════════════════════
 -- ── CONSULTA 1: LEFT JOIN ─────────────────
 -- Pregunta de negocio: ¿Qué productos del catálogo nunca fueron vendidos?
 -- Mostrá todos los productos y sus ventas asociadas.
 -- Los productos sin ventas aparecerán con NULL en las columnas de ventas.
--- [Tu consulta aquí]
+
+ SELECT
+    p.producto_id,
+    p.nombre,
+    p.categoria,
+    p.precio,
+    v.venta_id,
+    v.cantidad,
+    v.fecha_venta
+FROM productos p
+LEFT JOIN ventas v
+    ON p.producto_id = v.producto_id
+     WHERE v.venta_id IS NULL;
+
 -- ── CONSULTA 2: RIGHT JOIN ────────────────
 -- Pregunta de negocio: ¿Existen ventas registradas con productos
 -- que no figuran en nuestro catálogo? (posible error de carga de datos)
 -- Los registros huérfanos aparecerán con NULL en las columnas de productos.
--- [Tu consulta aquí]
+
+SELECT
+    p.producto_id,
+    p.nombre,
+    p.categoria,
+    v.venta_id,
+    v.producto_id AS venta_producto_id,
+    v.cliente_id,
+    v.cantidad,
+    v.fecha_venta
+FROM productos p
+RIGHT JOIN ventas v
+    ON p.producto_id = v.producto_id
+WHERE p.producto_id IS NULL;
+
 -- ── CONSULTA 3: FULL OUTER JOIN ───────────
 -- Pregunta de negocio: Vista completa de auditoría que muestre
 -- todos los productos y todas las ventas sin perder ninguna fila,
 -- identificando tanto productos sin ventas como ventas sin producto.
--- [Tu consulta aquí]
+
+SELECT
+    p.producto_id,
+    p.nombre,
+    p.categoria,
+    p.precio,
+    v.venta_id,
+    v.producto_id AS venta_producto_id,
+    v.cliente_id,
+    v.cantidad,
+    v.fecha_venta
+FROM productos p
+FULL OUTER JOIN ventas v
+    ON p.producto_id = v.producto_id;
+
+
+
+
+
+
+
